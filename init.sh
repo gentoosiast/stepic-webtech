38ffa25e2e6b1533334c9fad2697d0e95d489ebe
Serge Koksharov <gentoosiastic@gmail.com>
2016-11-05 13:23:18 +0300

    Fixed init script to work properly

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
